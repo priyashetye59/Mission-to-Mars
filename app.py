4e1ba5592e254006354609ebefd1b0cd96a29fcb
Priya <priyashetye59@gmail.com>
2022-11-08 02:10:11 -0500

    Final commit to submit.

--- app.py
+++ app.py
@@ -1,39 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
-
-# In[2]:
-
-
 app = Flask(__name__)
-
-
-# In[3]:
-
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-
-# In[4]:
-
-
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
     return render_template("index.html", mars=mars)
-
-
-# In[5]:
-
 
 @app.route("/scrape")
 def scrape():
@@ -42,16 +21,8 @@ def scrape():
     mars.update_one({}, {"$set": mars_data}, upsert=True)
     return redirect('/', code=302)
 
-
-# In[6]:
-
-
 if (__name__ == "__main__"):
     app.run()
-
-
-# In[ ]:
-
 
 
 
